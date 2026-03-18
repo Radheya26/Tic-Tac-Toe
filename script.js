b51f4a5e5eb9f0a5a699f25d7cdbde4511db8d10
Pritam Pramanik <70059343+Radheya26@users.noreply.github.com>
2026-03-18 18:38:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/*class Game{
+  
+  play() {
+    }
+  update(input) {
+    
+     }
+  draw() {
+     }
+  
+}*/
+window.onload = function(){
+  
+
+var item1 = document.querySelector("item1");
+var item2 = document.querySelector("item2");
+var item3 = document.querySelector("item3");
+var item4 = document.querySelector("item4");
+var item5 = document.querySelector("item5");
+var item6 = document.querySelector("item6");
+var item7 = document.querySelector("item7");
+var item8 = document.querySelector("item8");
+var item9 = document.querySelector("item9");
+
+
+function add(){
+  item1.innerHTML = "x";
+  
+}
+
+
+
+}
